5f906347c1642793abb80ac8d609166ac9a3dc70
Ali Shehzad <princealishehzad943@gmail.com>
2025-09-25 20:15:22 +0500

    Update router.ts

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.json({ message: "Hello from Docker v3 🎉" }));
+router.get("/", (req, res) => {
+  res.json({ message: "Hello from Docker v3 🎉" });
+});
 
+// ✅ Proper health check endpoint
 router.get("/health", (req, res) => {
-  throw new Error("Internal Server Error");
   res.status(200).json({ message: "Everything is good here 👀" });
 });
 
